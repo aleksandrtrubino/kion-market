423031ab2970baffd5102eccafc3e613ef8ffd50
aleksandrtrubino <trubino2003@gmail.com>
2024-03-17 21:52:47 +0300

    Initial commit

---
+++ src/main/java/ru/vistar/Main.java
@@ -0,0 +1,53 @@
+package ru.vistar;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void databaseUpdate() throws Exception {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/kion-market","postgres","postgres")) {
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            try (Liquibase liquibase = new liquibase.Liquibase("config/liquibase/master.xml", new ClassLoaderResourceAccessor(), database);) {
+                liquibase.update(new Contexts(), new LabelExpression());
+            }
+        } catch (DatabaseException e) {
+            throw e;
+        } catch (SQLException | LiquibaseException e) {
+            throw e;
+        }
+
+    }
+
+    public static void main(String[] args) throws SQLException {
+        //startDatabase();
+
+        try {
+            databaseUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("br.com.fredericci.pu");
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    }
+
+//    private static void startDatabase() throws SQLException {
+//        new Server().runTool("-tcp", "-web", "-ifNotExists");
+//    }
+}
